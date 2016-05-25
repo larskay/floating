6987e4f99fff2b1eef44cd2c81b7fc58bc3217c4
Lars Kay <lars@antenor.no>
2016-05-25 17:24:38 +0300

    Added mail account

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,14 @@ Floating::Application.configure do
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
-
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.live.com",
+    :port                 => 587,
+    :user_name            => 'monicamvik@hotmail.com',
+    :password             => 'Flytetank1',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  
+  }
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
