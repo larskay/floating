6e5ea9dbf1bf93e86f4aa66c6056473ae765c410
Lars Kay <lars@antenor.no>
2016-05-25 17:45:19 +0300

    Added mail account

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Floating::Application.initialize!
+ActionMailer::Base.smtp_settings = {
+:address              => "smtp.live.com",
+:port                 => 587,
+:user_name            => 'monicamvik@hotmail.com',
+:password             => 'Flytetank1',
+:authentication       => 'plain',
+:enable_starttls_auto => true
+:openssl_verify_mode => 'none' 
+}
